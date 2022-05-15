23e76c9edd0719a3452a463cae9012522f25f814
koldanJS <koldan.js@gmail.com>
2022-05-15 17:27:59 +0300

    task adding fixed

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.use('/api/timesSheet', require('./routes/timesSheet.routes'))
 app.use('/api/archive', require('./routes/archive.routes'))
 app.use('/api/activeItem', require('./routes/activeItem.routes'))
 
-// if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build/index.html'))
     })
-// }
+}
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 5000
 
 async function start() {
     try {
